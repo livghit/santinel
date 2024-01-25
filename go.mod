f5802c6ba5cdb07accb186fe06ab626bf15e16c4
livghit <liviu.g01@gmail.com>
2024-01-25 21:12:57 +0100

    Working on the server and implementing Gorm

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/livghit/santinel
 
 go 1.21.6
+
+require (
+	github.com/gorilla/mux v1.8.1
+	gorm.io/gorm v1.25.5
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,5 +1,27 @@
 package server
 
-// HTTP Server (using Gin) - MVC like pattern
+import (
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+)
+
+// HTTP Server (using Standard Library and Mux)
 // the server will be responsible also for frontend
-// things like tables , forms and so on via HTMX . Also using Alpinejs
+// things like tables , forms and so on via HTMX and ALPINEJS
+
+type Server struct {
+	*mux.Router
+}
+
+func NewServer(db *gorm.DB) *Server {
+	s := &Server{
+		Router: mux.NewRouter(),
+	}
+	s.routes()
+
+	return s
+}
+
+func (s *Server) routes() {
+	// Here register the routes
+}
